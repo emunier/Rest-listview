9513bd0ac4226c14b35a592f99832e47c7d7b552
emunier <emunier@ezee-compta.fr>
2023-03-23 15:15:09 +0100

    Initial commit

---
+++ app/src/main/java/org/libreapps/rest/ConnectionRest.java
@@ -0,0 +1,111 @@
+package org.libreapps.rest;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.libreapps.rest.obj.Product;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+public class ConnectionRest extends AsyncTask<String, Void, String> {
+    private final static String URL = "RENSEIGNER_URL_API";
+    private JSONObject jsonObj = null;
+
+    @Override
+    protected String doInBackground(String... strings) {
+        try {
+            return get(strings[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String get(String methode) throws IOException, JSONException {
+        String url = URL + "/";
+        InputStream is = null;
+        String parameters = "";
+        if(!methode.equals("POST")&&(jsonObj!=null)){
+            url += jsonObj.getInt("id");
+        }
+        if(jsonObj != null){
+            if(methode.equals("PUT")){
+                jsonObj.remove("id");
+            }
+            parameters  = "data="+URLEncoder.encode(jsonObj.toString(), "utf-8");
+        }
+
+        try {
+            final HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod(methode);
+
+            // Pour les methode POST et PUT on envoie les parametre avec l'OutputStreamWriter
+            if(methode.equals("POST")||methode.equals("PUT")){
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+                out.write(parameters);// here i sent the parameter
+                out.close();
+            }else{
+                conn.setDoInput(true);
+                conn.connect();
+            }
+
+            is = conn.getInputStream();
+            // Lit le InputStream et l'enregistre dans une string
+            return readIt(is);
+        } finally {
+            // Pour etre sur que le InputStream soit ferme apres avoir quitter l'application
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    private String readIt(InputStream is) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(is));
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = r.readLine()) != null) {
+            response.append(line).append('\n');
+        }
+        return response.toString();
+    }
+
+    public ArrayList<Product> parse(final String json) {
+        try {
+            final ArrayList<Product> products = new ArrayList<>();
+            final JSONArray jProductArray = new JSONArray(json);
+            for (int i = 0; i < jProductArray.length(); i++) {
+                products.add(new Product(jProductArray.optJSONObject(i)));
+            }
+            return products;
+        } catch (JSONException e) {
+            Log.v("TAG","[JSONException] e : " + e.getMessage());
+        }
+        return null;
+    }
+
+    public void setObj(JSONObject jsonObj){
+        this.jsonObj = jsonObj;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
